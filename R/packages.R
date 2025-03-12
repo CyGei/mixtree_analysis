3f6fc7ab8a6eddbe468f730cae11da639234aaa1
CyGei <c.geismar21@imperial.ac.uk>
2025-03-12 19:56:17 +0000

    update semantics for CRAN package version

--- R/packages.R
+++ R/packages.R
@@ -15,4 +15,3 @@ pacman::p_load(
 )
 pacman::p_load_gh("CyGei/simulacr") # to simulate outbreaks
 pacman::p_load_gh("CyGei/o2ools") #helper functions for outbreaker2
-pacman::p_load_gh("CyGei/epitree")
