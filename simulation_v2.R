5d19045b93546c3f46e14d3691326ab83235134c
cygsmr <c.geismar21@imperial.ac.uk>
2025-08-08 15:30:19 +0100

    simulation with n_cases strictly

--- simulation_v2.R
+++ simulation_v2.R
@@ -12,7 +12,7 @@ params_A <- build_params(
   off_k = 0.3,
   gt_mu = 3,
   gt_sd = 1.5,
-  population = 100,
+  n_cases = 100,
   duration = 365,
   n_trees = 100
 )
@@ -33,13 +33,13 @@ mixtree::tree_test(
 )
 
 params_B <- with(params_A, build_params(
-  off_R      = offspring_dist$parameters$R,
-  off_k      = 1e5,
-  gt_mu      = generation_time$parameters$mu,
-  gt_sd      = generation_time$parameters$sd,
-  population = population,
-  duration   = duration,
-  n_trees    = n_trees
+  off_R = offspring_dist$parameters$R,
+  off_k = 1e5,
+  gt_mu = generation_time$parameters$mu,
+  gt_sd = generation_time$parameters$sd,
+  n_cases = n_cases,
+  duration = duration,
+  n_trees = n_trees
 ))
 
 
@@ -56,13 +56,13 @@ mixtree::tree_test(
 
 
 params_C <- with(params_A, build_params(
-  off_R      = offspring_dist$parameters$R,
-  off_k      = offspring_dist$parameters$k + 0.1,
-  gt_mu      = generation_time$parameters$mu,
-  gt_sd      = generation_time$parameters$sd,
-  population = population,
-  duration   = duration,
-  n_trees    = n_trees
+  off_R = offspring_dist$parameters$R,
+  off_k = offspring_dist$parameters$k + 0.1,
+  gt_mu = generation_time$parameters$mu,
+  gt_sd = generation_time$parameters$sd,
+  n_cases = n_cases,
+  duration = duration,
+  n_trees = n_trees
 ))
 # tree_A with params C
 forest_C <- build_forest(true_tree = tree_A, params = params_C)
@@ -78,110 +78,120 @@ mixtree::tree_test(
 # =================================================
 # Simulation
 # =================================================
+plan(multisession, workers = availableCores() - 2)
 
+# Parameter grid
 param_grid <- expand.grid(
-  off_R = 3,
+  off_R = c(1.5, 3, 5),
   off_k = c(0.1, 0.3, 1, 10, 1e5),
   gt_mu = 3,
   gt_sd = 1.5,
-  population = 100,
+  n_cases = 100,
   duration = 365,
   n_trees = 50,
   stringsAsFactors = FALSE
-) |> as_tibble()
-
-base_id <- param_grid |>
-  mutate(row_number = row_number()) |>
-  filter(off_R == 3, off_k == 1e5) |>
-  pull(row_number)
-
-base_tree <- param_grid |>
-  slice(base_id) |>
-  pmap(build_params) |>
-  pluck(1) |> # same as .[[1]]
-  build_tree()
-
-base_tree |>
-  slice(-1) |>
-  igraph::graph_from_data_frame(directed = TRUE) |>
-  ggraph::ggraph(layout = "tree") +
-  ggraph::geom_edge_link() +
-  ggraph::geom_node_point(col = "grey") +
-  ggraph::geom_node_text(aes(label = name), col = "blue") # Label nodes by their 'name' attribute (ID)
+) |>
+  as_tibble() |>
+  mutate(p_id = row_number())
 
 
-plan(multisession, workers = availableCores() - 2)
-
+# Build all forests (each with its own reference tree)
 system.time({
   forest_grid <- param_grid |>
     mutate(
-      params = pmap(param_grid, build_params),
-      forest = future_map(params, ~ build_forest(true_tree = base_tree, params = .),
+      # Exclude p_id as it's not an input of build_params
+      params = pmap(select(param_grid, -p_id), build_params),
+      # Each forest gets its own reference tree
+      reference_tree = future_map(params, ~ build_tree(.x),
+        .options = furrr_options(seed = TRUE)
+      ),
+      # Each forest built from its own reference tree
+      forest = future_map2(reference_tree, params,
+        ~ build_forest(true_tree = .x, params = .y),
         .options = furrr_options(seed = TRUE)
       )
     )
 })
-# user  system elapsed
-# 4.06    0.57   21.19
 
+# Time when params$population_size (would result in trees with varying n_cases)
+# user  system elapsed
+# 36.50    5.11   69.74
+
+# Time when params$n_cases (trees with all exactly n_cases)
+# user  system elapsed
+# 34.39    7.83  355.94
+saveRDS(forest_grid, "data/forest_grid.rds")
+
+# Pairwise comparison
+comparison_grid <- expand.grid(
+  p_A = forest_grid$p_id,
+  p_B = forest_grid$p_id,
+  stringsAsFactors = FALSE
+) |>
+  as_tibble() |>
+  # get unique comparisons (e.g. 1 vs 2 is same as 2 vs 1)
+  # but get all other combinations
+  #filter(p_A <= p_B) |>
+  mutate(
+    R_A = forest_grid$off_R[p_A],
+    k_A = forest_grid$off_k[p_A],
+    R_B = forest_grid$off_R[p_B],
+    k_B = forest_grid$off_k[p_B]
+  )
+
+# all pairwise tests
 system.time({
-  test_grid <- forest_grid |>
+  results_grid <- comparison_grid |>
     mutate(
-      p_value = map_dbl(
-        .x = forest,
+      p_value = future_map2_dbl(
+        .x = p_A,
+        .y = p_B,
         .f = ~ {
           sample_n <- 25
           suppressWarnings(
             mixtree::tree_test(
-              sample(forest_grid$forest[[base_id]], sample_n),
-              sample(.x, sample_n),
+              sample(forest_grid$forest[[.x]], sample_n),
+              sample(forest_grid$forest[[.y]], sample_n),
               method = "permanova",
               test_args = list(permutations = 500)
             )$`Pr(>F)` |> pluck(1)
           )
-        }
-      )
-    )  
-})
-# user  system elapsed 
-# 1.89    0.64    2.55 
-
-base_forest <- forest_grid$forest[[base_id]]
-system.time({
-test_grid <- forest_grid |>
-  mutate(
-    p_value = future_map_dbl(
-      .x = forest,
-      .f = ~ {
-        sample_n <- 25
-        suppressWarnings(
-          mixtree::tree_test(
-            sample(base_forest, sample_n),
-            sample(.x, sample_n),
-            method = "permanova",
-            test_args = list(permutations = 500)
-          )$`Pr(>F)` |> pluck(1)
-        )
-      },
-      .options = furrr_options(
-        globals = c("base_forest"),
-        seed = TRUE
+        },
+        .options = furrr_options(seed = TRUE)
       )
     )
-  )
 })
-#  user  system elapsed 
-# 3.14    0.42    7.56 
+results_grid
 
-test_grid |>
-  select(-c(params, forest)) |> 
-  ggplot(aes(x = off_k, y = p_value)) +
-  geom_line() +
-  geom_point(size = 2) +
-  geom_hline(aes(yintercept = 0.05), lty = "dashed")+
-  scale_x_log10()+
-  labs(
-    x = "Overdispersion parameter (k)",
-    y = "PERMANOVA p-value",
+results_grid |>
+  filter(k_B >= k_A) |>
+  mutate(significance = if_else(p_value < 0.05, "Significant", "Non-significant")) |>
+  ggplot(aes(x = factor(k_A), y = factor(k_B), fill = significance)) +
+  geom_tile(color = "white", linewidth = 0.5) +
+  # Draw a line using geom_segment() on the diagonal points
+  geom_segment(
+    data = results_grid |> filter(k_A == k_B),
+    aes(
+      x = as.numeric(factor(k_A)) - 0.5,
+      y = as.numeric(factor(k_B)) - 0.5,
+      xend = as.numeric(factor(k_A)) + 0.5,
+      yend = as.numeric(factor(k_B)) + 0.5
+    ),
+    color = "black",
+    linewidth = 0.25,
+    inherit.aes = FALSE
   ) +
-  theme_minimal()
+  facet_grid(
+    cols = vars(factor(R_B)),
+    rows = vars(factor(R_A, levels = sort(unique(results_grid$R_A), decreasing = TRUE))),
+    labeller = labeller(
+      .rows = ~ paste("R =", .x),
+      .cols = ~ paste("R =", .x)
+    )
+  ) +
+  scale_fill_manual(values = c("Significant" = "#d73027", "Non-significant" = "#4575b4")) +
+  labs(
+    x = "k (Forest A)",
+    y = "k (Forest B)",
+    fill = "Difference"
+  )
